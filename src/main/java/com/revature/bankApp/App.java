5c8ffc25dc546773a851705495eee80fb116e66c
[Stephen Hilson] <[stephenrhilson@gmail.com]>
2018-12-29 20:01:07 -0500

    Innitial commit

---
+++ src/main/java/com/revature/bankApp/App.java
@@ -0,0 +1,16 @@
+package com.revature.bankApp;
+
+
+
+/**
+ * Bank Interface abstraction
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      System.out.println("HELLO SANIC");     
+      
+    }
+}
